74e30e540449c19abd32c1857ee3d72ee520edca
Adrien Hobbs <adrien.hobbs@gmail.com>
2018-10-15 20:15:45 -0400

    fix imports

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,3 @@
-import Vue from 'vue'
 import VueOnIntersect from 'vue-on-intersect'
 import VueSyncLocation from 'vue-sync-location'
 
@@ -6,7 +5,7 @@ import LazyVideo from '../components/LazyVideo'
 import LazyVideoAsGIF from '../components/LazyVideoAsGIF'
 
 export default {
-  install: function() {
+  install: function(Vue) {
     Vue.use(VueOnIntersect)
     Vue.use(VueSyncLocation)
 
